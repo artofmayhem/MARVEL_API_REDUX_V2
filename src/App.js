c14a680a375f9a33ff4fdcf57ebe77a9968d1e7d
artofmayhem <chef_tony_miller@yahoo.com>
2021-05-01 13:39:23 -1000

    zindex and postion adjustments

--- src/App.js
+++ src/App.js
@@ -17,8 +17,7 @@ function App() {
             color: "white",
             textAlign: "center",
             boxShadow: '0 0 3vh deepskyblue',
-            position: 'absolute',
-            zIndex: '10'
+            zIndex: '1'
           }}
         >
           <h2 style={{ padding: "1.5vh 0" }}>
